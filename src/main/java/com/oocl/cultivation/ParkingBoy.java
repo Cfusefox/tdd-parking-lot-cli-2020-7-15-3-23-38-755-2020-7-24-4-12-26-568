943673daf5342a24f31c02708581c580fe8269bd
YORK ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGYO5@OOCL.COM>
2020-07-24 16:18:55 +0800

    [ParkingBoy][York] feature: should_return_car_ticket_when_parking_car_into_paring_lot_given_car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -2,10 +2,13 @@ package com.oocl.cultivation;
 
 public class ParkingBoy {
 
+    private ParkingLot parkingLot;
+
     public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
     }
 
     public CarTicket parkingCar(Car car) {
-        return null;
+        return this.parkingLot.park(car);
     }
 }
