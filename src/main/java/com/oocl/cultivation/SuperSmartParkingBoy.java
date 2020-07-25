54312119793b9908d3e7a52bd1742d3d7dfb7a14
fox <1036674617@qq.com>
2020-07-26 00:38:56 +0800

    [SuperSmartParkingBoy][York] feature: add should_return_true_when_parking_car_into_parking_lot_given_four_car

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -14,16 +14,49 @@ public class SuperSmartParkingBoy implements ParkingBoy{
 
     @Override
     public CarTicket parkingCar(Car car) {
+        this.errorMessage = null;
+        float maxPercentage = maxPercentageOfUseableVolume(parkingLots);
+        for (ParkingLot parkingLot: parkingLots) {
+            if((parkingLot.getVolume() - parkingLot.getParkingRoom().size()) / (float) parkingLot.getVolume()  == maxPercentage) {
+                CarTicket carTicket = parkingLot.park(car);
+                if(carTicket != null) {
+                    return carTicket;
+                }
+                this.errorMessage = parkingLot.getErrorMessage();
+            }
+        }
         return null;
     }
 
     @Override
     public Car fetchCar(CarTicket carTicket) {
+        for (ParkingLot parkingLot: parkingLots) {
+            Car car = parkingLot.fetch(carTicket);
+            if(car != null) {
+                return car;
+            }
+            this.errorMessage = parkingLot.getErrorMessage();
+        }
         return null;
     }
 
     @Override
     public String answerCustomerMessage(CarTicket carTicket) {
-        return null;
+        if(this.errorMessage != null) {
+            return this.errorMessage;
+        } else {
+            return "Please provide your parking ticket.";
+        }
+    }
+
+    private float maxPercentageOfUseableVolume(ArrayList<ParkingLot> parkingLots) {
+        float max = 0;
+        for (ParkingLot parkingLot: parkingLots) {
+            int nullVolume = parkingLot.getVolume() - parkingLot.getParkingRoom().size();
+            if(max < nullVolume / (float) parkingLot.getVolume()) {
+                max = nullVolume / (float) parkingLot.getVolume();
+            }
+        }
+        return max;
     }
 }
