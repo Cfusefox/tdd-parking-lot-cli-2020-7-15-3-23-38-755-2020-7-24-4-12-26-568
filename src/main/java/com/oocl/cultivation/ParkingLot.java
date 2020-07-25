ae985145f33ccd0e0d0e8f219d790633cbda74c8
fox <1036674617@qq.com>
2020-07-25 12:01:43 +0800

    [ParkingLot][York] feature: add should_return_null_when_fetch_car_from_parking_lot_given_wrong_ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,11 +1,18 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+
 public class ParkingLot {
+
+    HashMap<CarTicket, Car> parkingRoom = new HashMap<>();
+
     public CarTicket park(Car car) {
-        return new CarTicket(car);
+        CarTicket carTicket = new CarTicket(car);
+        this.parkingRoom.put(carTicket, car);
+        return carTicket;
     }
 
     public Car fetch(CarTicket carTicket) {
-        return carTicket.getCar();
+        return this.parkingRoom.remove(carTicket);
     }
 }
