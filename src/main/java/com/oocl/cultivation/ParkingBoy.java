8a804fc377a0f41dfd3f9d79e6b07de9e3df5800
fox <1036674617@qq.com>
2020-07-27 20:11:04 +0800

    [York] refactor: delete file ParkingBoy.java

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++
@@ -1,7 +0,0 @@
-package com.oocl.cultivation;
-
-public interface ParkingBoy {
-    public CarTicket parkingCar(Car car);
-    public Car fetchCar(CarTicket carTicket);
-    public String answerCustomerMessage(CarTicket carTicket);
-}
