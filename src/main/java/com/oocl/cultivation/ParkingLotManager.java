6e28e7bdb79909d805f3243f355f14722907959f
fox <1036674617@qq.com>
2020-07-27 20:42:42 +0800

    [York] refactor: refactor ParkingLotManager

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -1,46 +1,29 @@
 package com.oocl.cultivation;
 
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ParkingLotManager {
 
+    List<ParkingCarBehavior> parkingAbles;
 
-    public ParkingLotManager(ArrayList<ParkingLot> parkingLots) {
-        this.parkingLots = parkingLots;
-        this.parkingBoys = new ArrayList<>();
-        this.parkingCarBehavior = new GeneralParkingCarBehavior();
+    public ParkingLotManager(ParkingCarBehavior... parkingAbles) {
+        this.parkingAbles = Arrays.asList(parkingAbles);
     }
 
-    public boolean addParkingBoy(ParkingBoy parkingBoy) {
-        return this.parkingBoys.add(parkingBoy);
+    public boolean addParkingBoy(ParkingCarBehavior parkingBoy) {
+        return this.parkingAbles.add(parkingBoy);
     }
 
-    public CarTicket letParkingBoyParkingCar(Car car) {
-        ParkingBoy parkingBoy = this.parkingBoys.get(0);
-        return parkingBoy.parkingCar(car);
-    }
 
-    public Car letParkingBoyFetchCar(CarTicket carTicket) {
-        ParkingBoy parkingBoy = this.parkingBoys.get(0);
-        return parkingBoy.fetchCar(carTicket);
-    }
 
     public CarTicket parkingCar(Car car) {
-        return this.parkingCarBehavior.parkingCar(car, this.parkingLots);
+        return this.parkingAbles.get(0).parkingCar(car);
     }
 
     public Car fetchCar(CarTicket carTicket) {
-        for (ParkingLot parkingLot : parkingLots) {
-            Car car = parkingLot.fetch(carTicket);
-            if (car != null) {
-                return car;
-            }
-        }
         return null;
     }
 
-    public String answerCustomerMessage() {
-        return parkingBoys.get(0).answerCustomerMessage(null);
-    }
 }
