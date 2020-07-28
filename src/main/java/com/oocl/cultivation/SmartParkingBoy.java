ed6cf91894a748743c45e081fb7b578713b4542b
ZHANGYO5 <york.zhang@oocl.com>
2020-07-28 12:39:21 +0800

    [York] refactor: refactor delete unused import

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,9 +3,7 @@ package com.oocl.cultivation;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class SmartParkingBoy extends GeneralParkingBoy {
 
