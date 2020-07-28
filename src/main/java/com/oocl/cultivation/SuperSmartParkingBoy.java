3247bcd171dfd6b2075ac168e988629ab9eb3800
ZHANGYO5 <york.zhang@oocl.com>
2020-07-28 09:18:16 +0800

    [York] refactor: refactor SuperSmartParkingBoy

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -2,6 +2,8 @@ package com.oocl.cultivation;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class SuperSmartParkingBoy extends SmartParkingBoy {
 
@@ -13,29 +15,13 @@ public class SuperSmartParkingBoy extends SmartParkingBoy {
 
     @Override
     public CarTicket parkingCar(Car car) {
-        float maxPercentage = maxPercentageOfUseableVolume(parkingLots);
-        for (ParkingLot parkingLot : parkingLots) {
-            if ((parkingLot.getVolume() - parkingLot.getParkingRoom().size()) / (float) parkingLot.getVolume() == maxPercentage) {
-                CarTicket carTicket = parkingLot.parkingCar(car);
-                if (carTicket != null) {
-                    return carTicket;
-                }
+        Optional<ParkingLot> optionalParkingLot = parkingLots.stream().max(Comparator.comparing(ParkingLot::getAvailablePercentage));
+        if (optionalParkingLot.isPresent()) {
+            if (optionalParkingLot.get().getAvailableLocations() > 0) {
+                this.errorMessage = "Not enough position.";
             }
         }
-        this.errorMessage = "Not enough position.";
-        return null;
-    }
-
-
-    private float maxPercentageOfUseableVolume(ArrayList<ParkingLot> parkingLots) {
-        float max = 0;
-        for (ParkingLot parkingLot : parkingLots) {
-            int nullVolume = parkingLot.getVolume() - parkingLot.getParkingRoom().size();
-            if (max < nullVolume / (float) parkingLot.getVolume()) {
-                max = nullVolume / (float) parkingLot.getVolume();
-            }
-        }
-        return max;
+        return optionalParkingLot.map(parkingLot -> parkingLot.parkingCar(car)).orElse(null);
     }
 
 
